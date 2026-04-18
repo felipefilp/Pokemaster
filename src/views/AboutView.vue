a6c357046f2d3e9081f9748f9cc1f4b2d47365cd
felipefilp <felipe_m_souza@yahoo.com.br>
2026-04-17 21:06:16 -0300

    correcoes sobre

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -4,20 +4,39 @@
       <h1 class="text-center">PokeVite</h1>
       <div class="card-body">
         <h2>Objetivo:</h2>
-        <p>Esse é mais um projeto desenvolvido para fins didáticos com intúito de praticar o desenvolvimento com Vue 3 utilizando Vite e consumindo a API PokeAPI.</p>
-        <hr>
+        <p>
+          Esse é mais um projeto desenvolvido para fins didáticos com intúito de
+          praticar o desenvolvimento com Vue 3 utilizando Vite e consumindo a
+          API PokeAPI.
+        </p>
+        <hr />
         <h3>Tutorial</h3>
         <p>
-          Link do GitHub: <a href="https://github.com/yurimarcon/Vite-pokedex" target="_blank" alt="Link do projeto no GitHub" >/Vite-pokedex</a>
-        </p>    
+          Link do GitHub:
+          <a
+            href="https://github.com/felipefilp/Pokemaster"
+            target="_blank"
+            alt="Link do projeto no GitHub"
+            >/Pokemaster</a
+          >
+        </p>
 
-        <p class="mt-2">Agradeço as pessoas que acompanharam a incentivaram o projeto!</p>
-        <p>Caso queira desenvolver acompanhando as video aulas e fique com alguma dúvida, fique a vontade para entrar em contato via e-mail.</p>
+        <p class="mt-2">
+          Agradeço as pessoas que acompanharam a incentivaram o projeto!
+        </p>
         <p>
-          <a class="btn btn-primary" href="mailto:felipe_m_souza@yahoo.com.br">📧 felipe_m_souza@yahoo.com.br</a>
+          <a class="btn btn-primary" href="mailto:felipe_m_souza@yahoo.com.br"
+            >📧 felipe_m_souza@yahoo.com.br</a
+          >
         </p>
         <p>Me acompanhe no Github e no Linkedin!</p>
-        <a class="btn btn-primary" target="_blank" href="https://link-service.netlify.app/" alt="Minhas mídias sociais">👨‍💻 Minhas mídias sociais 🤓 ✌️</a>
+        <a
+          class="btn btn-primary"
+          target="_blank"
+          href="https://www.linkedin.com/in/felipe-mart-souza/"
+          alt="Linkedin"
+          >Linkedin 🤓 ✌️</a
+        >
       </div>
     </div>
   </div>
